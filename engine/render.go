a92b861987b943f754d7c04b343e269982123c17
devitodbug <davidochiengy@gmail.com>
2026-03-15 17:57:16 +0300

    feat: improve rendered ui

--- engine/render.go
+++ engine/render.go
@@ -12,24 +12,28 @@ func (e *Engine) RenderTerminal() {
 
 	// Loop the mf so by row and print the columns
 	for row := RowNumber - 1; row >= 0; row-- {
-		fmt.Printf("%v %v", utils.Red, row+1)
+		fmt.Printf("%v %v", utils.Green, row+1)
 		fmt.Printf("%v ", utils.White)
 		for col := range ColumnNumber {
 			piece := e.board[row][col]
 			if piece == nil {
 				fmt.Printf(" . ")
 			} else {
-				fmt.Printf(" %c ", GetRenderLetter(piece.Type, piece.Color))
+				if piece.Type == King && e.isSquareAttacked(int32(col), int32(row), toggleCurrentPlayer(piece.Color)) {
+					fmt.Printf("%v %c %v", utils.Red, GetRenderLetter(piece.Type, piece.Color), utils.White)
+				} else {
+					fmt.Printf(" %c ", GetRenderLetter(piece.Type, piece.Color))
+				}
 			}
 		}
-		fmt.Printf("%v %v", utils.Red, row+1)
+		fmt.Printf("%v %v", utils.Green, row+1)
 		fmt.Printf("%v ", utils.White)
 		fmt.Println()
 	}
 
 	fmt.Printf("   ")
 	for num := range ColumnNumber {
-		fmt.Printf("%v %c ", utils.Red, 'a'+num)
+		fmt.Printf("%v %c ", utils.Green, 'a'+num)
 	}
 	fmt.Printf("%v\n", utils.White)
 }
